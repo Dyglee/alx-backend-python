c25bd409c5c7e73a1b7b45318ce940e7eb01f2ff
Yassine <yassinemoussafir31@gmail.com>
2024-08-01 11:00:58 +0000

    1-concat.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+"""
+This module provides a function for adding two float numbers.
+"""
+
 
 def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers 
+
+    Returns:
+    float: The sum of the two float numbers.
+    """
     return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module provides a function for concatenating two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings
+
+    Returns:
+    str: The concatenated string.
+    """
+    return str1 + str2
